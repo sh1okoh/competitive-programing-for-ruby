7872c5a9f469138fbbf03486903ac4ea622bb7e9
塩沢　光貴 <ko.shiozawa@a1706-036.opt-sys.local>
2019-10-27 16:42:07 +0900

    LeetCodeのreverse_integerを追加

---
+++ LeetCode/easy/reverse_integer.rb
@@ -0,0 +1,9 @@
+def reverse(x)
+  max_integer = 2147483647
+  minimum_integer = -2147483647
+  ans = x < 0 ? x.to_s.reverse.to_i * -1 : x.to_s.reverse.to_i
+  if ans < minimum_integer || max_integer < ans
+    return 0
+  end
+  return ans
+end
