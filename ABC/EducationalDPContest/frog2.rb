cbc691d0f49cfada9377333184362b3bc361c65f
塩沢　光貴 <ko.shiozawa@a1706-036.opt-sys.local>
2019-10-29 10:19:34 +0900

    リポジトリ名とか変更

--- ABC/EducationalDPContest/frog2.rb
+++ ABC/EducationalDPContest/frog2.rb
@@ -1,1 +1,14 @@
+def solve
+  n, k = gets.split.map!(&:to_i)
+  h = gets.split.map!(&:to_i)
+  dp = []
+  dp[0] = 0
+  dp[1] = (h[0] - h[1]).abs
+  (2..n-1).each do |i|
+    x = dp[i + 1] + (h[i] - h[i + 1]).abs
+    y = dp[i + 2] + (h[i] - h[i + 2]).abs
+    z = dp[i-1]
+  end
+end
+solve
 
