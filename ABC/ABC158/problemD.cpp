68f92e79d8cccb3f2c486f7ae68954c4814d90ce
塩沢　光貴 <ko.shiozawa@a1706-036.opt-sys.local>
2020-04-17 01:13:28 +0900

    study cpp

---
+++ ABC/ABC158/problemD.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+// using ll = long long;
+// using p = pair<int, int>;
+
+int main() {
+  // string s;
+  // cin >> s;
+  // int q;
+  // cin >> q;
+  // string t;
+  // rep(qi, q) {
+  //   int ty;
+  //   cin >> ty;
+  //   if (ty == 1) {
+  //     swap(s, t);
+  //   } else {
+  //     int f; char c;
+  //     cin >> f >> c;
+  //     if (f == 1) {
+  //       t += c;
+
+  //     }
+  //   };
+  // }
+  cout << "hoge" << endl;
+  return 0;
+}
