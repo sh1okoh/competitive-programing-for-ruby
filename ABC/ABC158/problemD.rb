8fa1dcecc6520b082e8efa498f1e93d9b38dc3d4
塩沢　光貴 <ko.shiozawa@a1706-036.opt-sys.local>
2020-04-16 23:30:30 +0900

    new problems

---
+++ ABC/ABC158/problemD.rb
@@ -0,0 +1,31 @@
+# def solve
+#   s = gets.chomp
+#   q = gets.to_i
+#   arr = []
+#   (0..q - 1).each do |i|
+#     arr.push(gets.split)
+#   end
+#
+#   (0..arr.length - 1).each do |i|
+#     if arr[i][0] == '1'
+#
+#     end
+#   end
+
+  # (0..arr.length - 1).each do |i|
+  #   if arr[i][0] == '1'
+  #     s.reverse
+  #   else
+  #     if arr[i][1] == '1'
+  #       s = arr[i][2] + s
+  #     else
+  #       s = s + arr[i][2]
+  #     end
+  #   end
+  #   p s
+  # end
+#
+#   print s
+# end
+#
+# solve
--- ABC/ABC160/problemD.rb
+++ ABC/ABC160/problemD.rb
@@ -1,1 +1,7 @@
-
+def solve
+  n, x, y = gets.split.map!(&:to_i)
+  x = x - 1
+  y = y - 1
+  ans = n
+end
+solve
