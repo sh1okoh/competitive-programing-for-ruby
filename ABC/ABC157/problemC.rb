a185fc346c01c813d14af141af81bebae3bdbed2
Koki Shiozawa <kooooo.0719task@gmail.com>
2020-04-19 23:26:26 +0900

    new problems

---
+++ ABC/ABC157/problemC.rb
@@ -0,0 +1,12 @@
+def solve
+    n, m = gets.split.map!(&:to_i)
+    arr = []
+    target = []
+    (0..m-1).each do |i|
+        arr.push(gets.split.map!(&:to_i))
+    end
+
+    (10**n).
+end
+
+solve
