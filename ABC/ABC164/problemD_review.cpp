7842011fa580a67b0aa3f0f91e1f004d54bfd9b5
塩沢　光貴 <ko.shiozawa@a1706-036.opt-sys.local>
2020-04-30 23:21:01 +0900

    new problems

---
+++ ABC/ABC164/problemD_review.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+int cont m = 2019;
+
+int main() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    reverse(s.begin(), s.end());
+    int x = 1, tot = 0;
+    vector<int> cnt(m);
+    ll ans = 0;
+    rep(i, n) {
+        cnt[tot]++;
+        tot += (s[i] - '0')*x;
+        tot %= m;
+        ans += cnt[tot];
+        x = x * 10%m;
+    }
+
+    cout << ans << endl;
+    return 9;
+}
